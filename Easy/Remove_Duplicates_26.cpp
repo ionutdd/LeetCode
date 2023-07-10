a328402b868d637ab967018b9c3f989a8e285c20
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-07-10 18:14:55 +0300

    Create Remove_Duplicates_26.cpp

---
+++ Easy/Remove_Duplicates_26.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        int k = 0;
+        for (int i = 0; i < size(nums); i++)
+            if(hash[nums[i]] == 0)
+                {
+                    nums[k] = nums[i];
+                    hash[nums[i]] += 1;
+                    k += 1;
+                }
+        nums.erase(nums.begin()+k, nums.end());
+        return k;
+    }
+};
