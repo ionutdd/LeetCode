b565d9c6050be7d96f792059ad9575b313189db0
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-07-27 08:08:52 +0300

    Create Symmetric_Tree_101.cpp

---
+++ Easy/Symmetric_Tree_101.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/symmetric-tree/
+//15 minutes
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        
+        return isSameTree(root->left, root->right);
+    }
+private:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+
+        if (p == nullptr || q == nullptr) {
+            return false;
+        }
+
+        return (p->val == q->val) && isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
+    }
+};
