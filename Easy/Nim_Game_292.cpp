ce64229b73e75f93c31d63f50634ec0728402a18
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-08-07 12:42:57 +0300

    Create Nim_Game_292.cpp

---
+++ Easy/Nim_Game_292.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/nim-game/
+//7 minutes
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n <= 3)
+            return true;
+        if (n % 4 == 0)
+            return false;
+        return true;
+    }
+};
