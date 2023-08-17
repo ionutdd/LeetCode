fb6d497e02f7207746bb711e6c7f19b17f33546c
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-08-17 11:06:17 +0300

    Update Index_Of_First_Occurence_28.cpp

--- Easy/Index_Of_First_Occurence_28.cpp
+++ Easy/Index_Of_First_Occurence_28.cpp
@@ -4,12 +4,12 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        unordered_map < int , string> hash;
-        hash[1] = needle;
+        unordered_map <string , int> hash;
+        hash[needle] = 1;
         if (size(needle) > size(haystack))
             return -1;
         for (int i = 0; i < size(haystack) - size(needle) + 1; i++)
-            if (haystack.substr(i, size(needle)) == hash[1])
+            if (hash.find(haystack.substr(i, size(needle))) != hash.end())
                 return i;
         return -1;
     }
